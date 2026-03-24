aaecaa2800cfcb8924a8de22aab2a3783c3a9d5e
John Pork <hde-andr@student.42porto.com>
2026-03-24 18:25:53 +0000

    main, organizations and shizz like that

---
+++ src/maze_gen.py
@@ -0,0 +1,36 @@
+from typing import Tuple, Optional
+import random
+
+
+class Cell:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+        self.directions = {
+            "N": True,
+            "E": True,
+            "S": True,
+            "W": True,
+        }
+        self.checked = True
+
+
+class MazeGenerator:
+    def __init__(
+            self,
+            width: int,
+            height: int,
+            entry: Tuple[int, int],
+            exit: Tuple[int, int],
+            perfect: bool,
+            output: str,
+            seed: Optional[int]
+            ) -> None:
+        self.width = width
+        self.height = height
+        self.entry = entry
+        self.exit = exit
+        self.perfect = perfect
+        self.output = output
+        self.grid = [[Cell(x, y) for x in range(width)] for y in range(height)]
+        self.seed = seed
--- src/parsing.py
+++ src/parsing.py
@@ -2,5 +2,9 @@ def read_map(filename):
     with open(filename, "r") as r:
         return [line.strip() for line in r]
 
+
 def parse_map(lines):
     return [list(line) for line in lines]
+
+
+print(read_map("option.txt"))
