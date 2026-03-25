80616d4b95b9b7989b5e201987a72c80234c3fe6
Cesar <cesarrtmartins@gmail.com>
2026-03-25 13:48:43 +0000

    New change

--- src/maze_gen.py
+++ src/maze_gen.py
@@ -40,16 +40,5 @@ class MazeGenerator:
         self.perfect = perfect
         self.output = output
         self.grid = [[Cell(x, y) for x in range(width)] for y in range(height)]
-<<<<<<< HEAD
         self.seed = seed
     
-=======
-        self.seed = random.randint(1, 1000000)
-
-    def generate():
-        """John pork"""
-        pass
-
-    def solve():
-        pass
->>>>>>> ca92ea8c2e07f0250c6aed900fc3fe8d2789240a
